be16c37e5ef74d9ceb3039cd4d3f9a114f2968b1
ntorba <32570754+ntorba@users.noreply.github.com>
2021-06-16 22:00:59 -0400

    Update __init__.py

--- network_simulations/__init__.py
+++ network_simulations/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for network-simulations."""
 
 __author__ = """Nick Torba"""
-__email__ = 'audreyr@example.com'
-__version__ = '0.1.0'
+__email__ = "torba@temple.edu"
+__version__ = "0.1.0"
